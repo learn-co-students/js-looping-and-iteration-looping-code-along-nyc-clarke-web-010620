e2c00d3aa852ab525acf82bdd5475d845fb29aa1
arichards4814 <arichards4814@gmail.com>
2020-02-16 11:14:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+
+function writeCards(cards){
+    let arr = []
+    for (let i = 0; i < cards.length; i++) {
+        arr.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+    }
+
+    return arr;
+}
+
+
+function countDown(num){
+
+    for (let i = num; i > -1; i--){
+        console.log(i)
+    }
+
+
+}
